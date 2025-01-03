7f0e421e4ccb6c829854adca5c302a1a51acdfae
NathanGrondin <scdonalduck@gmail.com>
2025-01-03 11:51:38 -0500

    added prettier and ran script to fix file format

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,15 +1,15 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
 const pool = new Pool({
-    connectionString: "postgresql://user:pass@localhost:5432/booking-backend",
-});
+  connectionString: 'postgresql://user:pass@localhost:5432/booking-backend',
+})
 
-export const db = drizzle(pool);
+export const db = drizzle(pool)
 
 export async function initializeDatabase() {
-    console.log('initializing db')
-    await db.execute(`
+  console.log('initializing db')
+  await db.execute(`
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         username TEXT NOT NULL UNIQUE,
@@ -17,5 +17,5 @@ export async function initializeDatabase() {
         email TEXT NOT NULL UNIQUE,
         role TEXT NOT NULL
     );
-  `);
+  `)
 }
--- src/database/repositories/users.ts
+++ src/database/repositories/users.ts
@@ -1,26 +1,29 @@
-import {db} from "../db";
-import {users} from "../schema";
-import {and, eq} from "drizzle-orm";
+import { db } from '../db'
+import { users } from '../schema'
+import { and, eq } from 'drizzle-orm'
 
 export interface user {
-    username: string;
-    password: string;
-    email: string;
-    role: string;
+  username: string
+  password: string
+  email: string
+  role: string
 }
 
 export const addUser = async (user: user): Promise<void> => {
-    const {username, password, email, role} = user;
-    await db.insert(users).values({username, password, email, role});
-};
-
-export const getUserByUsernamePassword = async (username: string, password: string): Promise<user | null> => {
-
-    const result = await db.select().from(users).where(and(eq(users.username, username), eq(users.password, password)))
-    if (result.length < 1){
-        throw new Error("Could not find user");
-    }
-    return result[0]
+  const { username, password, email, role } = user
+  await db.insert(users).values({ username, password, email, role })
 }
 
-
+export const getUserByUsernamePassword = async (
+  username: string,
+  password: string
+): Promise<user | null> => {
+  const result = await db
+    .select()
+    .from(users)
+    .where(and(eq(users.username, username), eq(users.password, password)))
+  if (result.length < 1) {
+    throw new Error('Could not find user')
+  }
+  return result[0]
+}
